fe5d21c40d1b1882a998742460398fd1375756a7
Dinesh <dineshsirvi01@gmail.com>
2017-07-28 01:16:03 +0530

    Init project

---
+++ README.md
@@ -0,0 +1,4 @@
+## Notice Board
+
+The app is specifically created for colleges to provide notifications and updates to students. It uses node.js and express as backend.
+  
---
+++ server/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
