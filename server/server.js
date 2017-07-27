47d207921f7b4274c48969701fa0e41202ae9caf
Dinesh <dineshsirvi01@gmail.com>
2017-07-28 01:22:33 +0530

    Initial setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var app = express();
+const app = express();
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+  res.send('Initial setup');
+});
+
+app.listen(3000, ()=>{
+  console.log("server is up on port 3000");
+});
